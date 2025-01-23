03df67f56390b05c6dc500cd4539081933fa5add
Ravi kumar <kravikumar9885@gmail.com>
2025-01-23 18:44:05 +0530

    updated code

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
-public class HelloWorld{
-    public static void main(String args[]){
-        System.out.prinln("Hello Ravi kumar");
-    }
+public class HelloWorld {
+
+public static void main(String[] args) {
+
+System.out.println("Hello, World");
+
+}
+
 }
